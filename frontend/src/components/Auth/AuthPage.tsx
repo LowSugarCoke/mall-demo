2203178b56cbe61b468a6fcf70491c89d96b75b1
Jack Lee <Jack.cy.Lee@viewsonic.com>
2023-06-26 13:57:57 +0800

    feat: add user api

--- frontend/src/components/Auth/AuthPage.tsx
+++ frontend/src/components/Auth/AuthPage.tsx
@@ -1,17 +1,44 @@
 import React, { useState } from 'react';
 import AuthForm from './AuthForm';
+import { UserApi } from '../API/UserApi';
+import { useNavigate } from 'react-router-dom';
 
 function AuthPage() {
     const [isLogin, setIsLogin] = useState(true);
+    const navigate = useNavigate();
 
-    const handleSubmit = (credentials: { email: string, password: string }) => {
+    const handleSubmit = async (credentials: { email: string, password: string }) => {
+        const { email, password } = credentials;
+
         // Call either login or register API here
         if (isLogin) {
             // Call login API
             console.log('Logging in:', credentials);
+            try {
+                const success = await UserApi.userLogin(email, password);
+                if (success) {
+                    console.log('Login successful');
+                    navigate('/products');
+                } else {
+                    console.log('Login failed');
+                }
+            } catch (error) {
+                console.error('An error occurred during login:', error);
+            }
         } else {
             // Call register API
             console.log('Registering:', credentials);
+            try {
+                const success = await UserApi.userRegister(email, password);
+                if (success) {
+                    console.log('Registration successful');
+                    alert('Registration successful');
+                } else {
+                    console.log('Registration failed');
+                }
+            } catch (error) {
+                console.error('An error occurred during registration:', error);
+            }
         }
     };
 
